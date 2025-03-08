ce739683bd133a0c67fd0bc0db25e02f62b77505
N-Friederich <127681326+N-Friederich@users.noreply.github.com>
2025-03-08 23:04:13 +0100

    Fix .item() graph break

--- src/transformers/models/sam/modeling_sam.py
+++ src/transformers/models/sam/modeling_sam.py
@@ -535,7 +535,7 @@ class SamMaskDecoder(nn.Module):
         output_tokens = torch.cat([self.iou_token.weight, self.mask_tokens.weight], dim=0)
         output_tokens = output_tokens.repeat(batch_size, point_batch_size, 1, 1)
 
-        if sparse_prompt_embeddings.sum().item() != 0:
+        if sparse_prompt_embeddings.sum() != torch.tensor(0):
             tokens = torch.cat((output_tokens, sparse_prompt_embeddings), dim=2)
         else:
             tokens = output_tokens
